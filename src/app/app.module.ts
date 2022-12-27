2138f8602e1d014e2e83cfa5f170cbf8f7bba79a
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-12-27 11:18:49 -0300

    dentro de  empleado-list creamos count-empleados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmpleadoListComponent } from './components/empleado-list/empleado-list.component';
+import { CountEmpleadosComponent } from './components/empleado-list/count-empleados/count-empleados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoListComponent
+    EmpleadoListComponent,
+    CountEmpleadosComponent
   ],
   imports: [
     BrowserModule

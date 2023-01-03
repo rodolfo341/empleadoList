8d519dd57c4b5cd81bcfff9004f96afdaba7c6c6
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2023-01-03 11:31:18 -0300

    .

---
+++ src/app/models/empleado.ts
@@ -0,0 +1,24 @@
+export class empleado {
+    /*
+    legajo: number;
+    nombre: string;
+    apellido: string;
+    sexo: string;
+    salario: number;
+    */
+
+    constructor( legajo: number,
+        nombre: string,
+        apellido: string,
+        sexo: string,
+        salario: number){
+        
+        /*
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+        this.salario = salario;
+        */
+    }
+}

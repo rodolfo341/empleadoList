3ae3644fd6095156363f716128aa83b5de7ff627
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2023-01-03 12:47:41 -0300

    .

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -9,7 +9,10 @@ import { Empleado } from 'src/app/models/empleado';
 })
 export class EmpleadoListComponent implements OnInit {
     listEmpleados: Empleado[] = [
-
+        { legajo: 1, nombre:'Homero', apellido: 'Simpsom', sexo: 'm', salario: 100000 },
+        { legajo: 2, nombre:'Mongomery', apellido: 'Bear', sexo: 'm', salario: 900000 },
+        { legajo: 3, nombre:'Carl', apellido: 'Lewis', sexo: 'm', salario: 800000 },
+        { legajo: 4, nombre:'Profesora', apellido: 'Cravaple', sexo: 'f', salario: 800000 }
     ]
     constructor() { }
 

c5ba79c7b788b7eaa3294bf6288921c47ae67120
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2023-01-23 20:21:28 -0300

    .

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -9,10 +9,11 @@ import { Empleado } from 'src/app/models/Empleado';
 })
 export class EmpleadoListComponent implements OnInit {
     listEmpleados: Empleado[] = [
-        { legajo: 1, nombre:'Homero', apellido: 'Simpsom', sexo: 'm', salario: 100 },
-        { legajo: 2, nombre:'Mongomery', apellido: 'Bear', sexo: 'm', salario: 90 },
-        { legajo: 3, nombre:'Carl', apellido: 'Lewis', sexo: 'm', salario: 80 },
-        { legajo: 4, nombre:'Profesora', apellido: 'Cravaple', sexo: 'f', salario: 70 }
+        { legajo: 1, nombre:'Homero', apellido: 'Simpsom', sexo: 'Masculino', salario: 100 },
+        { legajo: 4, nombre:'Lisa', apellido: 'Simpsom', sexo: 'Femenino', salario: 30 },
+        { legajo: 2, nombre:'Mongomery', apellido: 'Bear', sexo: 'Masculino', salario: 90 },
+        { legajo: 3, nombre:'Carl', apellido: 'Lewis', sexo: 'Masculino', salario: 80 },
+        { legajo: 4, nombre:'Profesora', apellido: 'Cravaple', sexo: 'Femenino', salario: 70 }
     ]
     constructor() { }
 
@@ -21,6 +22,15 @@ export class EmpleadoListComponent implements OnInit {
     obtenerTotalEmpleados(): number {
         return this.listEmpleados.length;
     }
+
+    obtenerTotalFemeninos(): number {
+        return this.listEmpleados.filter( list => list.sexo === 'Femenino').length;
+    }
+
+    obtenerTotalMasculino(): number {
+        return this.listEmpleados.filter( list => list.sexo === 'Masculino').length;
+    }
+
 }
 
 
